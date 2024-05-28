83856e61a871012de61d34eb2f5a2fdabb17bba0
stuart938503 <github@stuartandnatalie.uk>
2024-05-28 09:50:53 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -12,3 +12,36 @@ function Update-PSProfile {
         Write-Debug $_
     }
 }
+
+function Add-GraphPermissionsToManagedIdentity()
+{
+    param(
+        [string]$TenantId,
+        [string]$ManagedIdentityName,
+        [string[]]$Permissions
+    )
+
+    # Connect to Microsoft Graph in the specified tenant
+    Write-Host "Connecting to Microsoft Graph..."
+    if((Get-MgContext).TenantId -ne $TenantId) {
+        Connect-MgGraph -TenantId $TenantId -Scopes "Application.Read.All","AppRoleAssignment.ReadWrite.All,RoleManagement.ReadWrite.Directory"
+    }
+
+    # Get the Managed Identity and Graph service principals
+    Write-Host "Getting service principals..."
+    $GraphPrincipal = Get-MgServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
+    $ManagedIdentityPrincipal = Get-MgServicePrincipal -Filter "DisplayName eq '$ManagedIdentityName'"
+
+    # Add the permissions to the Managed Identity
+    foreach($Permission in $Permissions) {
+        Write-Host "Adding permission $Permission..."
+        $AppRole = $GraphPrincipal.AppRoles | Where-Object {$_.Value -eq $Permission -and $_.AllowedMemberTypes -contains "Application"}
+        New-MgServicePrincipalAppRoleAssignment -PrincipalId $ManagedIdentityPrincipal.Id -ServicePrincipalId $ManagedIdentityPrincipal.Id -ResourceId $GraphPrincipal.Id -AppRoleId $AppRole.Id > $null
+    }
+    
+}
+
+function Get-PublicIp {
+    ((Invoke-WebRequest -Uri "https://api.ipify.org?format=json").content | ConvertFrom-Json).ip
+    ((Invoke-WebRequest -Uri "https://api64.ipify.org?format=json").content | ConvertFrom-Json).ip
+}
