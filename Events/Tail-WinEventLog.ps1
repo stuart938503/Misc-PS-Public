52dff9b8bf4744aecb933eb63505d96933248220
stuart938503 <github@stuartandnatalie.uk>
2024-06-17 09:22:59 +0100

    Update Tail-WinEventLog.ps1

--- Events/Tail-WinEventLog.ps1
+++ Events/Tail-WinEventLog.ps1
@@ -1,12 +1,21 @@
 Function Tail-WinEventLog {
     Param(
-        [hashtable]$FilterHashTable
+        [hashtable]$FilterHashTable,
+        [string[]]$Properties,
+        [switch]$FormatAsList
     )
 
-    $LatestEvent = Get-WinEvent -FilterHashtable $FilterHashTable -MaxEvents 1
-    $LastTimeCreated = $LatestEvent.TimeCreated
-    $LastRecordId = $LatestEvent.RecordId
-    $FilterHashTable.Add("StartTime",$LastTimeCreated)
+    #Add a StartTime if not passed in
+    if (-not $FilterHashTable.ContainsKey("StartTime")) {
+        $LatestEvent = Get-WinEvent -FilterHashtable $FilterHashTable -MaxEvents 1
+        $LastTimeCreated = $LatestEvent.TimeCreated
+        $LastRecordId = $LatestEvent.RecordId
+        $FilterHashTable.Add("StartTime",$LastTimeCreated)
+    }
+
+    if (-not $PSBoundParameters.ContainsKey("Properties")) {
+        $Properties = "TimeCreated","Id","LevelDisplayName","Message"
+    }
 
     while($True) {
         $Events = Get-WinEvent -FilterHashtable $FilterHashTable | ? RecordId -gt $LastRecordId | Sort TimeCreated -Descending
@@ -15,7 +24,11 @@ Function Tail-WinEventLog {
             $LastTimeCreated = $Events[0].TimeCreated
             $LastRecordId = $Events[0].RecordId
 
-            $Events
+            if($FormatAsList) {
+                $Events | Select-Object -Property $Properties | Format-List
+            } else {
+                $Events | Select-Object -Property $Properties | Format-Table -AutoSize
+            }
         }
 
         Start-Sleep -Seconds 1
